41f8059d1a391290fcaa7001459cde459c01a9af
Mustapha OTSMANE <fafa.ots15@gmail.com>
2025-12-12 21:29:32 +0100

    restarted project 💔

--- ev-backend/src/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-	// Fallback status code
-	const status = err.status || 500;
-	console.error('Error handler:', err); // keep lean log
-	res.status(status).json({
-		success: false,
-		error: err.message || 'Something went wrong',
-	});
-};
